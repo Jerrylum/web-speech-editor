ee4e59dd5e10708bd88c098b0c50f4d1f52b3309
Jerrylum <serverofjerry@gmail.com>
2022-02-02 23:45:03 +0800

    improve coding style

--- src/Algorithm.ts
+++ src/Algorithm.ts
@@ -39,18 +39,18 @@ export function check_is_google_chrome(): boolean {
 
     // Edge (based on chromium) detection
     // @ts-ignore
-    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") !== -1);
 
     return !isOpera && !isFirefox && !isSafari && !isIE && !isEdge && !isEdgeChromium;
 }
 
 export function diff_algorithm(raw_input: string[], split_eng_word = true) {
-    if (raw_input.length == 0) return [];
+    if (raw_input.length === 0) return [];
 
     const root_idx = raw_input.reduce(
         (ans_idx, me, me_idx) => (me.length > raw_input[ans_idx].length) ? me_idx : ans_idx,
         0);
-    if (raw_input[root_idx].length == 0) return [];
+    if (raw_input[root_idx].length === 0) return [];
 
     let input = raw_input;
 
@@ -63,19 +63,19 @@ export function diff_algorithm(raw_input: string[], split_eng_word = true) {
     while (index[root_idx] + batch_match_count < input[root_idx].length) {
         let str_0_char = input[root_idx][index[root_idx] + batch_match_count];
         if (input.every((val, i) =>
-            (val[index[i] + batch_match_count] == str_0_char) &&
+            (val[index[i] + batch_match_count] === str_0_char) &&
             (
                 split_eng_word ||
                 !(
-                    buf_eng != (buf_eng = /[A-Za-z]/.test(val[index[i] + batch_match_count])) &&
-                    buf_eng == true
+                    buf_eng !== (buf_eng = /[A-Za-z]/.test(val[index[i] + batch_match_count])) &&
+                    buf_eng === true
                 )
             ))) {
             batch_match_count++;
             continue;
         }
 
-        if (batch_match_count != 0)
+        if (batch_match_count !== 0)
             rtn.push([input[root_idx].substring(index[root_idx], index[root_idx] + batch_match_count)]);
         input.forEach((val, i) => index[i] += batch_match_count);
         batch_match_count = 0;
@@ -94,7 +94,7 @@ export function diff_algorithm(raw_input: string[], split_eng_word = true) {
                 let all_find = input.every((val, j) => {
                     find_index[j] = val.indexOf(base_str[k], index[j]);
                     find_total = Math.max(find_total, find_index[j] - index[j]);
-                    return find_index[j] != -1;
+                    return find_index[j] !== -1;
                 });
 
                 if (all_find && find_total < the_best_find_total) {
@@ -107,7 +107,7 @@ export function diff_algorithm(raw_input: string[], split_eng_word = true) {
             all_base_end -= (!(k < base_str.length)) ? 1 : 0;
         });
 
-        if (all_base_end == 0) {
+        if (all_base_end === 0) {
             break;
         }
 
@@ -118,7 +118,7 @@ export function diff_algorithm(raw_input: string[], split_eng_word = true) {
 
     let buf: string[] = [];
     input.forEach((val, i) => buf.push(val.substring(index[i])));
-    if (buf.every((val, i) => val == buf[0]))
+    if (buf.every((val, i) => val === buf[0]))
         rtn.push([buf[0]]);
     else
         rtn.push(buf);
